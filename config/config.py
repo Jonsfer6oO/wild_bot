ff0a0551fc734f1f9afdcd29c08d4b84c909c366
Иван Попов <popov-id06@mail.ru>
2024-06-28 15:12:34 +0700

    Админская панель

--- config/config.py
+++ config/config.py
@@ -7,6 +7,7 @@ class TgBot:
     bot_token:str
     admin_id:int
     chat_id:int
+    chat_main_id:int
 
 #конфигурационные данные
 @dataclass
@@ -23,6 +24,7 @@ conf = Config(
     tg_conf=TgBot(
         env("BOT_TOKEN"),
         env("ADMIN_ID"),
-        env("CHAT_ID")
+        env("CHAT_ID"),
+        env("CHAT_MAIN_ID")
         )
 )
--- hendlers/admin_hendlers.py
+++ hendlers/admin_hendlers.py
@@ -1,6 +1,7 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from time import sleep
+from  aiogram.types.poll_answer import PollAnswer
 
 from filter import Walid_admin, walid_quer
 from config import conf
@@ -8,25 +9,38 @@ from lexicon import text
 
 
 
+
 #инициализация роутера
 ad_r = Router()
 
-ad_r.message.filter(Walid_admin(conf))
+ad_r.message.filter(Walid_admin(conf), )
 
-
+list_poll = []
+status_admin = False
 
 #вход в панель администратора
 @ad_r.message(F.text.lower() == 'admin')
 async def walid_adm(message:Message):
+    global status_admin
     await message.answer(text = text['ADMIN_REG'])
+    status_admin = True
 
 #перессылка опроса
 @ad_r.message(walid_quer)
 async def return_poll(message:Message, bot):
+    global list_poll, status_admin
+    print(message.model_dump_json(indent=4))
 
-    if message.poll:
+    if message.poll and status_admin:
+
+        # Информирование об отправке опроса
+        await message.reply(text=text["SEND_POLL"])
+
+        # поступление нового опроса
         await bot.send_message(chat_id=conf.tg_conf.chat_id,
                                text=text["NEW_POLL"])
+
+        # создание опроса
         mess = await bot.send_poll(
                             chat_id=conf.tg_conf.chat_id,
                             question=message.poll.question,
@@ -34,20 +48,52 @@ async def return_poll(message:Message, bot):
                             is_anonymous=False
         )
 
-        sleep(600)
+        sleep(5)
         await bot.send_message(chat_id=conf.tg_conf.chat_id,
                                text=text["TEN_MIN"])
-        sleep(600)
+        sleep(5)
         await bot.send_message(chat_id=conf.tg_conf.chat_id,
                                text=text["TWEN_MIN"])
-        sleep(600)
+        sleep(5)
         await bot.send_message(chat_id=conf.tg_conf.chat_id,
                                text=text["EXIT_POLL"])
 
-
+        # остановка опроса
         await bot.stop_poll(chat_id=conf.tg_conf.chat_id,
                               message_id=mess.message_id)
 
+        # проверка на участие людей в опросе
+        if list_poll:
+
+            # отправка сообщения с пользователями
+            list_user_str = ' '.join(list_poll)
+            await bot.send_message(chat_id=conf.tg_conf.chat_id,
+                                text=text["ANSWER_POLL"].format(list_user_str))
+
+            # статистика
+            await message.answer(text=text['STATE'].format("@" + str(message.chat.username),
+                                                        len(list_poll),
+                                                        300//len(list_poll),
+                                                        300))
+        else:
+            # опроса не состоялся
+            await bot.send_message(chat_id=conf.tg_conf.chat_id,
+                                text=text["NO_POLL"])
+
+            await message.answer(text=text["NO_POLL"])
+
+        list_poll = []
+
+    else:
+        # отправка без админки
+        await message.answer(text=text['NOT_WALID'])
+
+# Составления списка пользователей участвоваших в опросе
+@ad_r.poll_answer()
+async def func(poll:PollAnswer):
+    print(poll)
+    if poll.user.username not in list_poll:
+        list_poll.append("@" + str(poll.user.username))
 
 
 #неизвестные команды
